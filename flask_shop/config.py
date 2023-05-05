b7179d168d062458aebb38c8d6d80780007c8d8e
PatrickHui <bobbypeter@126.com>
2023-05-05 14:14:43 +0800

    first commit

--- flask_shop/config.py
+++ flask_shop/config.py
@@ -4,11 +4,11 @@ class Config():
     # 配置MySQL参数
     MYSQL_DIALECT = 'mysql'
     MYSQL_DIRVER = 'pymysql'
-    MYSQL_NAME = 'Hui'
-    MYSQL_PWD = '18201750658Bobby'
+    MYSQL_NAME = 'your-name'
+    MYSQL_PWD = 'your-pasword'
     MYSQL_HOST = 'localhost'
     MYSQL_PORT = 3306
-    MYSQL_DB = 'flask_shop'
+    MYSQL_DB = 'your-db'
     MYSQL_CHARSET ='utf8mb4'
 
     SQLALCHEMY_DATABASE_URI = f'{MYSQL_DIALECT}+{MYSQL_DIRVER}://{MYSQL_NAME}:{MYSQL_PWD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?charset={MYSQL_CHARSET}'
